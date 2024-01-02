79d87f4967e6a00f216d01ec2007653dc0fcd64a
Alan Souza <alan.souza@guidance.dev>
2024-01-02 12:56:09 -0300

    feat: add basic theme

--- panda.config.ts
+++ panda.config.ts
@@ -1,3 +1,4 @@
+/* eslint-disable sort-keys/sort-keys-fix */
 import { defineConfig } from '@pandacss/dev';
 
 export default defineConfig({
@@ -14,6 +15,23 @@ export default defineConfig({
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    tokens: {
+      colors: {
+        primary: { value: '#010137' },
+        secondary: { value: '#b010d2' },
+      },
+      radii: {
+        sm: { value: '0.25rem' },
+        md: { value: '0.5rem' },
+        lg: { value: '1.5rem' },
+      },
+      spacing: {
+        xs: { value: '0.5rem' }, // 8px
+        sm: { value: '0.75rem' }, // 12px
+        md: { value: '1rem' }, // 16px
+        lg: { value: '1.5rem' }, // 24px
+        xl: { value: '2rem' }, // 32px
+      },
+    },
   },
 });
